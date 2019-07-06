3b4e0518aec7eff908a246a1af3bcd435849c760
nick <njrusmc@Gmail.com>
2019-07-06 08:35:24 -0400

    more openconfig/NXOS/netconfig and OC trees

--- m5/nxos.py
+++ m5/nxos.py
@@ -4,6 +4,7 @@ import json
 
 
 def main():
+
     with manager.connect(
         host="sbx-nxos-mgmt.cisco.com",
         port=10000,
@@ -11,11 +12,17 @@ def main():
         password="Admin_1234!",
         hostkey_verify=False,
         device_params={"name": "nexus"},
+        timeout=60,
     ) as conn:
-        resp = conn.get_config(source="running",
-            filter=("subtree", '<interfaces xmlns="http://openconfig.net/yang/interfaces"></interfaces>'))
+        resp = conn.get_config(
+            source="running",
+            filter=(
+                "subtree",
+                '<interfaces xmlns="http://openconfig.net/yang/interfaces"></interfaces>',
+            ),
+        )
         jresp = xmltodict.parse(resp.xml)
-        #print(json.dumps(jresp, indent=2))
+        print(json.dumps(jresp, indent=2))
 
         # this mess needs cleanup, but basically prints all switchport
         # info for access and trunk ports. no SVI or L3 ports
@@ -26,7 +33,7 @@ def main():
                 if switched_vlan:
                     eth = switched_vlan["config"]
                     mode = eth["interface-mode"].lower()
-                    print(f"Name: {intf['name']}  Type: {mode}", end="  ")
+                    print(f"Name: {intf['name']:<8}  Type: {mode:<6}", end="  ")
 
                     if mode == "access":
                         print(f"Access VLAN: {eth['access-vlan']}")
@@ -36,5 +43,42 @@ def main():
                         print(f"(no additional data)")
 
 
+def update_vlan(intf_name, vlan_id):
+    intf_to_update = {
+        "name": intf_name,
+        "ethernet": {
+            "@xmlns": "http://openconfig.net/yang/interfaces/ethernet",
+            "switched-vlan": {
+                "@xmlns": "http://openconfig.net/yang/vlan",
+                "config": {"access-vlan": str(vlan_id)},
+            },
+        },
+    }
+
+    # Assemble correct payload structure containing interface list, along
+    # with any other items to be updated
+    config_dict = {
+        "config": {  # also "data"
+            "interfaces": {
+                "@xmlns": "http://openconfig.net/yang/interfaces",
+                "interface": [intf_to_update],
+            }
+        }
+    }
+
+    xpayload = xmltodict.unparse(config_dict)
+    with manager.connect(
+        host="sbx-nxos-mgmt.cisco.com",
+        port=10000,
+        username="admin",
+        password="Admin_1234!",
+        hostkey_verify=False,
+        device_params={"name": "nexus"},
+    ) as conn:
+        config_resp = conn.edit_config(target="running", config=xpayload)
+    print(f"{intf_name} VLAN updated to {vlan_id}")
+
+
 if __name__ == "__main__":
+    update_vlan("eth1/124", 518)
     main()
