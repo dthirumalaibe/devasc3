8aef75badc9d20b5a67e4e9f98712d6f1eefb9d9
nick <njrusmc@Gmail.com>
2019-07-09 08:15:58 -0400

    restconf comments

--- m5/restconf_iosxe.py
+++ m5/restconf_iosxe.py
@@ -4,7 +4,7 @@
 """
 Author: Nick Russo
 Purpose: Using RESTCONF with IOS-XE specific YANG models to manage DHCP
-server pools on a Cisco OS-XE router via the always-on Cisco DevNet sandbox.
+server pools on a Cisco IOS-XE router via the always-on Cisco DevNet sandbox.
 """
 
 import json
@@ -25,27 +25,33 @@ def main():
     # I suspect this changes frequently. See here for more details:
     # https://developer.cisco.com/site/ios-xe
     api_path = "https://ios-xe-mgmt.cisco.com:9443/restconf"
+
+    # The specific path to update DHCP configuration per YANG model.
+    # You can explore using Postman/curl or through the YANG model directly.
     dhcp_target = "data/Cisco-IOS-XE-native:native/ip/dhcp"
 
+    # Create 2-tuple for "basic" authentication using Cisco DevNet credentials.
     auth = ("root", "D_Vay!_10&")
-    get_headers = {"Accept": "application/yang-data+json"}
-    post_headers = {
-        "Content-Type": "application/yang-data+json",
-        "Accept": "application/yang-data+json, application/yang-data.errors+json",
-    }
 
-    # Issue a GET request to collect a list of network objects configured
-    # on FTD. These are the IP subnets, hosts, and FQDNs that might be
-    # included in various security access policies.
+    # Define headers for issuing HTTP GET requests to receive YANG data as JSON.
+    get_headers = {"Accept": "application/yang-data+json"}
+
+    # Issue a GET request to collect the DHCP pool information only. This will
+    # return a list of dictionaries where each dictionary represents a pool.
     get_dhcp_response = requests.get(
-        f"{api_path}/{dhcp_target}",
+        f"{api_path}/{dhcp_target}/pool",
         headers=get_headers,
         auth=auth,
         verify=False,
     )
-    if get_dhcp_response.ok and get_dhcp_response.text:
-        dhcp_pools = get_dhcp_response.json()["Cisco-IOS-XE-native:dhcp"]
-        for pool in dhcp_pools["Cisco-IOS-XE-dhcp:pool"]:
+
+    # If the request succeed with a 220 "OK" message and there is
+    # some text defined, then step through the JSON and extract the useful
+    # bits of information. This is a good exercise to work on accessing data
+    # from complex JSON structured.
+    if get_dhcp_response.status_code == 200 and get_dhcp_response.text:
+        dhcp_pools = get_dhcp_response.json()["Cisco-IOS-XE-dhcp:pool"]
+        for pool in dhcp_pools:
             print(f"ID: {pool['id']}")
             print(f"  Domain: {pool['domain-name']}")
             print(f"  Network: {pool['network']['number']}")
@@ -59,6 +65,14 @@ def main():
     else:
         print("No DHCP pools currently configured")
 
+    # Create JSON structure to add a new pool along with the HTTP POST
+    # headers needed to add it. The JSON below represents this
+    # configuration:
+    # ip dhcp pool NICKTEST
+    #  network 192.0.2.0 255.255.255.0
+    #  default-router 192.0.2.254
+    #  dns-server 8.8.8.8 8.8.4.4
+    #  domain-name njrusmc.net
     add_pool = {
         "Cisco-IOS-XE-dhcp:pool": [
             {
@@ -70,7 +84,14 @@ def main():
             }
         ]
     }
+    post_headers = {
+        "Content-Type": "application/yang-data+json",
+        "Accept": "application/yang-data+json, application/yang-data.errors+json",
+    }
 
+    # Issue HTTP POST request to a similar URL used for the GET request,
+    # except carrying the new DHCP pool in the HTTP body. Also, we don't need
+    # to specify "/pool" since the dictionary key in the body carries it.
     post_dhcp_response = requests.post(
         f"{api_path}/{dhcp_target}",
         headers=post_headers,
@@ -78,8 +99,15 @@ def main():
         data=json.dumps(add_pool),
         verify=False,
     )
+
+    # HTTP 201 means "created", implying a new resource was added. The
+    # response will tell us the URL of the newly-created resource, simplifying
+    # future removal.
     if post_dhcp_response.status_code == 201:
-        print("Created new DHCP pool via RESTCONF")
+        print(f"Added DHCP pool at: {post_dhcp_response.headers['Location']}")
+
+        # Save configuration whenever the DHCP pool is added. This ensures
+        # the configuration will persist across reboots.
         post_save_response = requests.post(
             f"{api_path}/operations/cisco-ia:save-config",
             headers=post_headers,
