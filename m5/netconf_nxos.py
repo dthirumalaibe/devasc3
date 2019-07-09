4a213a868b7d091e990c9ad045501e338b23b73b
nick <njrusmc@Gmail.com>
2019-07-09 16:51:20 -0400

    cleaned up nxos netconf

--- m5/netconf_nxos.py
+++ m5/netconf_nxos.py
@@ -7,52 +7,93 @@ VLANs on a Cisco NX-OS switch via the always-on Cisco DevNet sandbox.
 """
 
 
-from ncclient import manager
 import xmltodict
-import json
+from ncclient import manager
 
 
 def main():
+    """
+    Execution begins here.
+    """
 
-    with manager.connect(
-        host="sbx-nxos-mgmt.cisco.com",
-        port=10000,
-        username="admin",
-        password="Admin_1234!",
-        hostkey_verify=False,
-        device_params={"name": "nexus"},
-        timeout=60,
-    ) as conn:
-        resp = conn.get_config(
-            source="running",
-            filter=(
-                "subtree",
-                '<interfaces xmlns="http://openconfig.net/yang/interfaces"></interfaces>',
-            ),
-        )
+    connect_params = {
+        "host": "sbx-nxos-mgmt.cisco.com",
+        "port": 10000,
+        "username": "admin",
+        "password": "Admin_1234!",
+        "hostkey_verify": False,
+        "allow_agent": False,
+        "look_for_keys": False,
+        "device_params": {"name": "nexus"},
+    }
+
+    with manager.connect(**connect_params) as conn:
+        print("NETCONF session connected")
+
+        # To save time, only capture 3 switchports. Less specific filters
+        # will return more information, but take longer to process/transport.
+        # Challenge to viewers: Can you use a loop to build the interface list?
+        nc_filter = """
+            <interfaces xmlns="http://openconfig.net/yang/interfaces">
+                <interface>
+                    <name>eth1/71</name>
+                </interface>
+                <interface>
+                    <name>eth1/72</name>
+                </interface>
+                <interface>
+                    <name>eth1/73</name>
+                </interface>
+            </interfaces>
+        """
+
+        # Execute a "get-config" RPC using the filter defined above
+        resp = conn.get_config(source="running", filter=("subtree", nc_filter))
+
+        # Parse the XML into a Python dictionary
         jresp = xmltodict.parse(resp.xml)
-        print(json.dumps(jresp, indent=2))
+        # import json; print(json.dumps(jresp, indent=2))
 
-        # this mess needs cleanup, but basically prints all switchport
-        # info for access and trunk ports. no SVI or L3 ports
+        # Iterate over all the interfaces returned by get-config
         for intf in jresp["rpc-reply"]["data"]["interfaces"]["interface"]:
-            switchport = intf.get("ethernet")
-            if switchport:
-                switched_vlan = switchport.get("switched-vlan")
-                if switched_vlan:
-                    eth = switched_vlan["config"]
-                    mode = eth["interface-mode"].lower()
-                    print(f"Name: {intf['name']:<8}  Type: {mode:<6}", end="  ")
 
-                    if mode == "access":
-                        print(f"Access VLAN: {eth['access-vlan']}")
-                    elif mode == "trunk":
-                        print(f"Native VLAN: {eth['native-vlan']}")
-                    else:
-                        print(f"(no additional data)")
+            # Declare a few local variables to make accessing data deep
+            # within the JSON structure a little easier
+            config = intf["ethernet"]["switched-vlan"]["config"]
+            mode = config["interface-mode"].lower()
+
+            # Print common switchport data
+            print(f"Name: {intf['name']:<7}  Type: {mode:<6}", end="  ")
+
+            # Print additional data depending on access vs trunk ports
+            if mode == "access":
+                print(f"Access VLAN: {config['access-vlan']}")
+            elif mode == "trunk":
+                print(f"Native VLAN: {config['native-vlan']}")
+            else:
+                print(f"(no additional data)")
+
+        # Make a change to the access VLAN on a single port
+        # Challenge for viewers: enhance it to take a collections of VLANs
+        # instead of just one at a time!
+        intf = "eth1/71"
+        vlan = 518
+        config_resp = update_vlan(conn, intf, vlan)
+        if config_resp.ok:
+            print(f"{intf} VLAN updated to {vlan}")
+
+    print("NETCONF session disconnected")
 
 
-def update_vlan(intf_name, vlan_id):
+def update_vlan(conn, intf_name, vlan_id):
+    """
+    Updates an existing switchport with a new VLAN ID. Expects that the
+    NETCONF connection is already open and that all data is valid. Feel
+    free to add more data validation here as a challenge.
+    """
+
+    # NETCONF edit-config RPC payload which defines interface to update.
+    # This follows the YANG model we picked apart in the get-config section.
     intf_to_update = {
         "name": intf_name,
         "ethernet": {
@@ -75,19 +116,13 @@ def update_vlan(intf_name, vlan_id):
         }
     }
 
+    # Assemble the XML payload by "unparsing" the JSON dict, then
+    # issue an edit-config RPC to the NX-OS device. Return the
+    # rpc-reply so that the caller can take action on the result.
     xpayload = xmltodict.unparse(config_dict)
-    with manager.connect(
-        host="sbx-nxos-mgmt.cisco.com",
-        port=10000,
-        username="admin",
-        password="Admin_1234!",
-        hostkey_verify=False,
-        device_params={"name": "nexus"},
-    ) as conn:
-        config_resp = conn.edit_config(target="running", config=xpayload)
-    print(f"{intf_name} VLAN updated to {vlan_id}")
+    config_resp = conn.edit_config(target="running", config=xpayload)
+    return config_resp
 
 
 if __name__ == "__main__":
-    update_vlan("eth1/124", 518)
     main()
