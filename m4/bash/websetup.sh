f022a8957f77db305be4e5ca24b8a7de17f10854
nick <njrusmc@Gmail.com>
2019-08-15 22:13:58 -0400

    m4 cleanup

--- m4/bash/websetup.sh
+++ m4/bash/websetup.sh
@@ -1,21 +1,20 @@
-# TASK 1: Update hostname (CLI arg)
+#!/bin/bash
+
+echo "TASK 1: Update hostname based on CLI argument"
 hostname "$1"
 
-# TASK 2: Install httpd
+echo "TASK 2: Install httpd"
 yum install -y httpd
 
-# TASK 3: Update HTML h1 header (CLI arg)
+echo" TASK 3: Update HTML h1 header (CLI arg)"
 sed -i "s:<h1>.*</h1>:<h1>$1</h1>:g" \
   /usr/share/httpd/noindex/index.html
 
-# TASK 4: Start httpd
+echo "TASK 4: Start httpd"
 systemctl start httpd
 
-# TASK 5: Add webadmin user
+echo "TASK 5: Add webadmin user"
 useradd -m -s /bin/bash webadmin
 echo webpass | passwd webadmin --stdin
 
-# TASK 6: Allow SSH password auth
-sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" \
-  /etc/ssh/sshd_config
-systemctl restart sshd
+echo "Done!"
