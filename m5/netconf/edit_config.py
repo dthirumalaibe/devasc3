ff6c938816731f7af253241553aff9c68a52fee6
nick <njrusmc@Gmail.com>
2019-08-17 11:05:40 -0400

    netconf updates

--- m5/netconf/edit_config.py
+++ m5/netconf/edit_config.py
@@ -39,55 +39,6 @@ def main():
     with manager.connect(**connect_params) as conn:
         print("NETCONF session connected")
 
-        # To save time, only capture 3 switchports. Less specific filters
-        # will return more information, but take longer to process/transport.
-        # Note: In this sandbox, it can take ~30 seconds to get all interfaces
-        # and several minutes to get the whole config, so be aware!
-        nc_filter = """
-            <interfaces xmlns="http://openconfig.net/yang/interfaces">
-                <interface>
-                    <name>eth1/71</name>
-                </interface>
-                <interface>
-                    <name>eth1/72</name>
-                </interface>
-                <interface>
-                    <name>eth1/73</name>
-                </interface>
-            </interfaces>
-        """
-
-        # Execute a "get-config" RPC using the filter defined above
-        resp = conn.get_config(source="running", filter=("subtree", nc_filter))
-
-        # Uncomment line below to see raw RPC XML reply; great for learning
-        # print(resp.xml)
-
-        # Parse the XML text into a Python dictionary
-        jresp = xmltodict.parse(resp.xml)
-
-        # Uncomment line below to see parsed JSON RPC; great for learning
-        # import json; print(json.dumps(jresp, indent=4))
-
-        # Iterate over all the interfaces returned by get-config
-        for intf in jresp["rpc-reply"]["data"]["interfaces"]["interface"]:
-
-            # Declare a few local variables to make accessing data deep
-            # within the JSON structure a little easier
-            config = intf["ethernet"]["switched-vlan"]["config"]
-            mode = config["interface-mode"].lower()
-
-            # Print common switchport data
-            print(f"Name: {intf['name']:<7}  Type: {mode:<6}", end="  ")
-
-            # Print additional data depending on access vs trunk ports
-            if mode == "access":
-                print(f"Access VLAN: {config['access-vlan']}")
-            elif mode == "trunk":
-                print(f"Native VLAN: {config['native-vlan']}")
-            else:
-                print(f"(no additional data)")
-
         # Perform the update, and if success, print a message
         config_resp = update_intf(conn, "intf_state.yml")
 
@@ -128,7 +79,7 @@ def update_intf(conn, filename):
     # Assemble correct payload structure containing interface list, along
     # with any other items to be updated
     config_dict = {
-        "config": {  # also "data"
+        "config": {
             "interfaces": {
                 "@xmlns": "http://openconfig.net/yang/interfaces",
                 "interface": intfs_to_update,
@@ -179,8 +130,9 @@ def save_config_nxos(conn):
     Reference the NX-OS programmability documentation for further details.
     """
 
-    save_rpc = """
-        <copy_running_config_src xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
+    save_xmlns = "http://cisco.com/ns/yang/cisco-nx-os-device"
+    save_rpc = f"""
+        <copy_running_config_src xmlns="{save_xmlns}">
              <startup-config/>
         </copy_running_config_src>
     """
