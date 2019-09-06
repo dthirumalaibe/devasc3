f9ea32b2fd59ff8954444e3d2b6c19fb49714fbe
nick <nickrus@cisco.com>
2019-09-06 01:01:58 -0400

    basic working pyats/genie

--- m4/pyats/README.md
+++ m4/pyats/README.md
@@ -3,3 +3,10 @@ These files are included for reference to give you a minimum
 working example of these powerful tools. This was not covered
 in the course as these tools are quite complex and certainly not
 beginner-level tools.
+
+## Sample runs
+In the `data_ref/` directory, there are two files.
+  * `passing_run.txt`: This shows an example where all tests pass. This
+    implies all OSPF neighbors are up
+  * `failing_run.txt`: This shows an example where some tests fail. I
+    broke one of the OSPF neighbors on purpose to simulate this failure.
--- m4/pyats/run_test.py
+++ m4/pyats/run_test.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python
 
+"""
+Author: Nick Russo
+Purpose: Simple demonstration of Cisco pyATS and Genie frameworks
+for reference.
+"""
+
 import json
 from pyats import aetest
 from genie.conf import Genie
 
 
 class NetworkTestcase(aetest.Testcase):
+    """
+    Object-oriented programming (OOP) design used to inherit (subclass)
+    from the main pyATS Testcase class, allowing us to define our own
+    custom tests.
+    """
 
     @aetest.setup
     def setup(self):
-        # Store variable for quick access to device list
-        self.devices = self.parameters["testbed"].devices
+        """
+        Runs before the tests begin.
+        Connect to all devices in the tested at once.
+        """
 
-        # Connect to all devices in the tested at once
         self.parameters["testbed"].connect()
 
     @aetest.test
     def test_ospf_neighbors(self):
-        for name, device in self.devices.items():
-            nbrs = json.dumps(device.parse("show ip ospf neighbor"), indent=2)
-            print(f"{name} OSPF neighbors:\n{nbrs}")
+        """
+        Test case asserts that all OSPF neighbors are up. Issues the
+        'show ip ospf neighbor' command and parses the output into structured
+        data for easy access/assertions.
+        """
+
+        # Loop over each device and check OSPF neighbors
+        for name, device in self.parameters["testbed"].devices.items():
+            ospf_nbrs = device.parse("show ip ospf neighbor")
+            ospf_nbrs_str = json.dumps(ospf_nbrs, indent=2)
+            print(f"{name} OSPF neighbors:\n{ospf_nbrs_str}")
+
+            # In a full-mesh triangle, each router should have 2 neighbors
+            assert len(ospf_nbrs["interfaces"]) == 2
 
 
 if __name__ == "__main__":
